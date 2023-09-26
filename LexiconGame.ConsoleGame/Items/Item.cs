a780a2b27aaec50650955f4eccddb4242ed6f5d0
DZM77 <32109758+DZM77@users.noreply.github.com>
2023-09-26 14:14:37 +0200

    Add Item

---
+++ LexiconGame.ConsoleGame/Items/Item.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LexiconGame.ConsoleGame.Items
+{
+    internal class Item : IDrawable
+    {
+        private readonly string name;
+        public ConsoleColor Color { get; }
+        public string Symbol { get; }
+
+        public Item(string symbol, ConsoleColor color, string name)
+        {
+            Symbol = symbol;
+            Color = color;
+            this.name = name;
+        }
+
+        public override string ToString() => name;
+        //{
+        //    return base.ToString();
+        //}
+
+
+    }
+}
