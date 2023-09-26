eec5db28a710c737fbb6b2f74ec996323b6726f0
DZM77 <32109758+DZM77@users.noreply.github.com>
2023-09-26 14:07:33 +0200

    IEnumerable<T>

--- LexiconGame.ConsoleGame/Program.cs
+++ LexiconGame.ConsoleGame/Program.cs
@@ -1,4 +1,25 @@
 ﻿
+//string someText = "Hej  på dig";
+
+//foreach (var item in someText)
+//{
+//    Console.WriteLine(item);
+//}
+
+
+
+//var backpack = new LimitedList<string>(6);
+//backpack.Add("Hej");
+
+
+//foreach (var item in backpack)
+//{
+//    Console.WriteLine(item);
+//}
+
+
+
+
 var game = new Game();
 game.Run();
 
--- LexiconGame.LimitedList/LimitedList.cs
+++ LexiconGame.LimitedList/LimitedList.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace LexiconGame.LimitedList
 {
-    public class LimitedList<T> 
+    public class LimitedList<T> : IEnumerable<T>
     {
         private readonly int capacity;
         private List<T> list;
@@ -27,5 +28,21 @@ namespace LexiconGame.LimitedList
             if(IsFull) return false;
             list.Add(item); return true;
         }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach(T item in list)
+            {
+
+                //Do somethin if needed?
+
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        //{
+        //    throw new NotImplementedException();
+        //}
     }
 }
